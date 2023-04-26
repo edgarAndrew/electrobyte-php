037326e54976b912ed9d35b19fed2badc56c02b5
edgarAndrew <edgarcamelo03@gmail.com>
2023-04-26 15:42:21 +0530

    added some images

---
+++ login.js
@@ -0,0 +1,74 @@
+console.clear();
+
+const loginBtn = document.getElementById('login');
+const signupBtn = document.getElementById('signup');
+
+loginBtn.addEventListener('click', (e) => {
+	let parent = e.target.parentNode.parentNode;
+	Array.from(e.target.parentNode.parentNode.classList).find((element) => {
+		if(element !== "slide-up") {
+			parent.classList.add('slide-up')
+		}else{
+			signupBtn.parentNode.classList.add('slide-up')
+			parent.classList.remove('slide-up')
+		}
+	});
+});
+
+signupBtn.addEventListener('click', (e) => {
+	let parent = e.target.parentNode;
+	Array.from(e.target.parentNode.classList).find((element) => {
+		if(element !== "slide-up") {
+			parent.classList.add('slide-up')
+		}else{
+			loginBtn.parentNode.parentNode.classList.add('slide-up')
+			parent.classList.remove('slide-up')
+		}
+	});
+});
+
+function show() {
+    var p = document.getElementById('pwd');
+    p.setAttribute('type', 'text');
+}
+
+function hide() {
+    var p = document.getElementById('pwd');
+    p.setAttribute('type', 'password');
+}
+
+var pwShown = 0;
+
+document.getElementById("eye").addEventListener("click", function () 
+{
+    if (pwShown == 0) {
+        pwShown = 1;
+        show();
+    } else {
+        pwShown = 0;
+        hide();
+    }
+}, false);
+
+$(document).ready(function() {
+    $('#loginBtn').click(function(event) {
+        var username = $('#username').val();
+        var password = $('#password').val();
+
+        if(username.length == 0) {
+            alert("Please enter username.");
+            event.preventDefault();
+            return false;
+        }
+
+        if(password.length == 0) {
+            alert("Please enter password.");
+            event.preventDefault();
+            return false;
+        }
+
+        // add other validation checks here
+
+        return true;
+    });
+});
